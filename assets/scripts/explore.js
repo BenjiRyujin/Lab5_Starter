71b7f81a92b4249eb221522e095054bd16e776a2
Benjamin Ryujin <55765860+BenjiRyujin@users.noreply.github.com>
2024-05-07 13:54:14 -0700

    Adding select dropdown

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,31 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const textToSpeakInput = document.getElementById('text-to-speak');
+  const voiceSelect = document.getElementById('voice-select');
+  const buttonSpeak = document.querySelector('button');
+  const imageFace = document.querySelector('img');
+
+  function populateVoiceList(){
+
+    /*
+    const voices = window.speechSynthesis.getVoices();
+    console.log(voices);
+    for (let i=0; i<voices.length; i++){
+      const pick = document.createElement('option');
+      //pick.textContent = '${voices[i].name}(${voices[i].lang})';
+    }
+    */
+    
+    voices.forEach(voice => {
+      const option = document.createElement('option');
+      option.textContent = voice.name;
+      option.setAttribute('data-lang', voice.lang);
+      option.setAttribute('data-name', voice.name);
+      voiceSelect.appendChild(option);
+    });
+    
+  }
+
+  populateVoiceList();
 }
